ec10d37a0844e63c0a2c72e704759c59bff62a23
Eldar Aslanbeily <eldar.aslanbeily@gmail.com>
2026-03-05 19:17:39 +0200

    Vendor Qaas.Mocker.CommunicationObjects into repo

---
+++ Qaas.Mocker.CommunicationObjects/ConfigurationObjects/Command/CommandResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+
+namespace Qaas.Mocker.CommunicationObjects.ConfigurationObjects.Command;
+
+[ExcludeFromCodeCoverage]
+public record CommandResponse
+{
+    public string Id { get; init; } = string.Empty;
+
+    public string ServerInstanceId { get; init; } = string.Empty;
+    
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public CommandType Command { get; init; }
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public Status Status { get; init; }
+    
+    public string? ExceptionMessage { get; init; }
+}
