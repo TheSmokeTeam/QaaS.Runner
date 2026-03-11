1e47633f0057ee881fe0656e6108db79529ec8b9
eldarush <eldarush@users.noreply.github.com>
2026-03-11 20:54:52 +0200

    test(e2e): simplify runner rabbit smoke coverage

--- QaaS.Runner.E2ETests/Generators/FromConsumerGenerator/FromConsumerGeneratorConfig.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace QaaS.Runner.E2ETests.Generators.FromConsumerGenerator;
-
-public record FromConsumerGeneratorConfig
-{
-    [Required] public string SessionName { get; set; } = string.Empty;
-    [Required] public string ConsumerName { get; set; } = string.Empty;
-}
